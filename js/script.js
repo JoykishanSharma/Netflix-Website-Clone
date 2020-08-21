0baf794a93c6441d15cbfa5dbc5a5406cb8375f0
JoykishanSharma <joykishan120@gmail.com>
2020-08-21 23:36:18 +0530

    updated script.js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-/** Script will be added soon */
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+
+        if (content.style.display === "block") {
+            var current_rotation = 270;
+            document.querySelector(".expand_icon").style.transform = 'rotate(' + current_rotation + 'deg)';
+            content.style.display = "none";
+        }
+        else {
+            var current_rotation = 135;
+            document.querySelector(".expand_icon").style.transform = 'rotate(' + current_rotation + 'deg)';
+            content.style.display = "block";
+        }
+    });
+}
